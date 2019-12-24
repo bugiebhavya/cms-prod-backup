ada0ace8d09d3524c0b1b7da08ace56ccbd84fa5
Python dev <pallavi@example.com>
2019-12-24 11:35:12 +0530

    Remove Settings wagtailCustomForm Commit

--- inter/settings/base.py
+++ inter/settings/base.py
@@ -174,9 +174,7 @@ WAGTAIL_SITE_NAME = "inter"
 BASE_URL = 'http://example.com'
 
 LOGIN_URL = '/us-login/'
-# AUTH_USER_MODEL = 'dashboard.Customer'
-# WAGTAIL_USER_EDIT_FORM = 'dashboard.forms.CustomUserEditForm'
-# WAGTAIL_USER_CREATION_FORM = 'dashboard.forms.CustomUserCreationForm'
-# WAGTAIL_USER_CUSTOM_FIELDS = ['avatar',]
+
+
 WAGTAILDOCS_DOCUMENT_MODEL = 'documents.CustomDocument'
-WAGTAILVIDEOS_VIDEO_MODEL = 'videos.CustomVideo'
+
