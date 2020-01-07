0ed274bfa999a2cc093dd251d60c025d699cbb6b
Python dev <codes@arkasoftwares.com>
2020-01-07 19:20:43 +0530

    durations

--- home/templatetags/duration_field.py
+++ home/templatetags/duration_field.py
@@ -8,10 +8,10 @@ register = template.Library()
 
 @register.filter(name='duration_field')
 def duration(td):
-	total_seconds = int(td.total_seconds())
-	return time.strftime("%H:%M:%S",time.gmtime(total_seconds))
-
-@register.filter(name='iduration_field')
-def iduration(td):
-	total_seconds = timedelta(microseconds=td).total_seconds()
-	return time.strftime("%H:%M:%S",time.gmtime(total_seconds))
+	try:
+		total_seconds = int(td.total_seconds())
+		return time.strftime("%H:%M:%S",time.gmtime(total_seconds))
+	except:
+		total_seconds = timedelta(microseconds=td).total_seconds()
+		return time.strftime("%H:%M:%S",time.gmtime(total_seconds))
+	
