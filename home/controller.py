2f095bd7f20eaff162bbc587dbd5b2c33c4b78e6
bugiebhavya <paliwalbhavya.bp@gmail.com>
2020-08-18 21:04:35 +0530

    3 tasks 18th aug

--- home/controller.py
+++ home/controller.py
@@ -2,8 +2,8 @@ from django.views.generic import TemplateView
 from django.shortcuts import render, get_object_or_404
 from wagtailvideos.models import Video 
 from django.views.generic import View 
-from .forms import AdminLoginForm, CommentForm
-from modules.users.models import User
+from .forms import AdminLoginForm, CommentForm, UserCreationForm, UserEditForm
+from modules.users.models import User, UserInterest
 from django.shortcuts import render, reverse 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages 
@@ -58,3 +58,96 @@ class CommentView(LoginRequiredMixin,APIView):
 			return Response({'message': 'Your comment posted successfully', 'code': 200, 'data': {'username': user.username.title(), 'body': comment.body, 'created': comment.created.strftime('%b. %d, %Y, %I:%M %P.')}})
 		except Exception as ex:
 			return Response({'code': 500, 'message': str(ex)})
+
+from .models import *
+from .forms import *
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.db import transaction
+import pdb
+
+class UserCreate(CreateView):
+	model = User
+	template_name = 'wagtailusers/users/create.html'
+	form_class = UserCreationForm
+	success_url = None
+
+	def get_context_data(self, **kwargs):
+		data = super(UserCreate, self).get_context_data(**kwargs)
+		allInterest = UserInterest.objects.all()
+		interestList = list(allInterest)
+		listObjects = []
+		if self.request.POST:
+			data['form'] = UserCreationForm(self.request.POST)
+			data['interestList'] = interestList
+			
+		else:
+			data['interests'] = UserInterestFormSet()
+			data['interestList'] = interestList
+		return data
+
+	def form_valid(self, form):
+		context = self.get_context_data()
+		form.instance.created_by = self.request.user
+		self.object = form.save(commit=False)
+		if form.is_valid():
+			user = form.save()
+			listObj = []
+			
+			for i in range(1, len(context['interestList'])+1):
+				print('percent: ',self.request.POST.get('percent-{}'.format(i)))
+				listObj.append(UserInterestPercent(user = user, interest = UserInterest.objects.get(name=self.request.POST.get('interest-{}'.format(i))),percent = self.request.POST.get('percent-{}'.format(i))))
+			UserInterestPercent.objects.bulk_create(listObj)
+		else:
+			data['interestList'] = interestList
+		return super(UserCreate, self).form_valid(form)
+
+	def get_success_url(self):
+		return reverse_lazy('wagtailusers_users:index')
+
+class UserUpdate(UpdateView):
+	model = User
+	template_name = 'wagtailusers/users/edit.html'
+	form_class = UserEditForm
+	success_url = None
+	userId = None
+
+	def get_context_data(self, **kwargs):
+		data = super(UserUpdate, self).get_context_data(**kwargs)
+		url = (self.request.build_absolute_uri(''))
+		userId = url.split('/')[-2]
+		allInterest = UserInterestPercent.objects.filter(user = User.objects.get(id = userId))
+		interestList = list(allInterest)
+		percentList = []
+		for i in range(len(interestList)):
+			percentList.append(interestList[i].percent)
+		res = dict(zip(interestList, percentList))   
+		if self.request.POST:
+			data['form'] = UserCreationForm(self.request.POST)
+			data['interestList'] = interestList
+			data['percentDict'] = self.request.POST.get('percentDict')
+			
+		else:
+			data['interestList'] = interestList
+			data['percentDict'] = res
+			print('hiii: ',data['percentDict'])
+		return data
+
+	def form_valid(self, form):
+		context = self.get_context_data()
+		form.instance.created_by = self.request.user
+		self.object = form.save(commit=False)
+		if form.is_valid():
+			user = form.save()
+			listObj = []
+			print('hello: ',context['percentDict'])
+			for i in range( len(context['interestList'])):
+				UserInterestPercent.objects.filter(id = context['interestList'][i].id).update(percent = self.request.POST.get('percent-{}'.format(i+1)))
+			# UserInterestPercent.objects.bulk_update(listObj, fields=['percent'])
+		else:
+			data['percentDict'] = context['percentDict']
+			return data
+		return super(UserUpdate, self).form_valid(form)
+
+	def get_success_url(self):
+		return reverse_lazy('wagtailusers_users:index')
